488c9f6dff6b9788d2fa22f85f639df15ac66b9b
leedawnn <2g_hee@naver.com>
2021-12-21 17:05:55 +0900

    :sparkles: feat: 영화 정보 API 불러와서 렌더링
    async-await 방식으로 API를 불러옴. 목록을 불러오면 로딩 상태가 false로 바뀜. 영화 제목, 장르, 별점, 런타임을 보여줌.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  return <div>{loading ? <h1>Loading...</h1> : null}</div>;
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  console.log(movies);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map(movie => (
+            <div key={movie.id}>
+              <h2>{movie.title_long}</h2>
+              <ul>
+                {movie.genres.map(g => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+              <h4>{movie.genres}</h4>
+              <h4>Rating: {movie.rating}</h4>
+              <h4>
+                Runtime: {Math.floor(movie.runtime / 60)}h {movie.runtime % 60}m
+              </h4>
+              <img src={movie.medium_cover_image} alt={movie.title} />
+              <p>{movie.summary}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
